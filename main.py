fd5705fd778124f33fa3f8d8ccb14411e4b8126b
Michael <mniebaue@fau.edu>
2025-04-01 04:55:55 -0400

    cloud build file update

--- main.py
+++ main.py
@@ -6,15 +6,5 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)  # This line is ignored when using Gunicorn
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=8080)  # This line is ignored when using Gunicorn
