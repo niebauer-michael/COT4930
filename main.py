acdf8721e16560330dcca47568abc85e3dd6a0b0
Michael <mniebaue@fau.edu>
2025-04-01 05:24:51 -0400

    cloud build file update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!!!!!!!!!!!"
+    return "Hello, World!!"
 
 if __name__ == "__main__":
     # Flask's built-in server running on port 8080
--- test_example.py
+++ test_example.py
@@ -4,5 +4,5 @@ from main import app
 def test_hello():
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"Hello, World!"
+    assert response.data == b"Hello, World!!"
     assert response.status_code == 200
