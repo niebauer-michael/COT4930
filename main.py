6506480749b4104a5a091a6eb5ad71053aba1671
Michael <mniebaue@fau.edu>
2025-04-01 23:56:12 -0400

    Updated Dockerfile and Cloud Build configuration

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-# main.py
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, Response
+from google.cloud import storage
+import random
+import string
+import json
+import requests
+from google.auth import compute_engine
+from google import genai
+from google.genai import types
+import google.auth
+from PIL import Image
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 
 API_KEY = 'AIzaSyDKEqNxdilZfuE-IFymWgVnfOpjXqjabUg'
+
+
 
 
 @app.route("/")
@@ -11,6 +25,11 @@ def hello():
     title = 'title'
     items = 'items'
     return render_template('index.html', title=title, items=items)
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    print('upload worked')
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     # Flask's built-in server running on port 8080
