34c65a12dbb05d882f8f6f6f4f640c3c4f0f841f
Michael <mniebaue@fau.edu>
2025-04-02 15:29:09 -0400

    Updated Dockerfile and Cloud Build configuration

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def saveJSONTOCloudStorage(randomName, json_data):
 
 def getImageDescription(file):
     image = Image.open(file)
+    image = image.resize((400, 400))
     model = genai.GenerativeModel('gemini-1.5-flash')
     # Generate content from an image and prompt
     response = model.generate_content([
